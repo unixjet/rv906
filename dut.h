5b7a5414cb5cfe8f1d82123b4ae5f7360cc73e9d
zhou <zhou.z.af@m.titech.ac.jp>
2026-08-20 03:07:29 +0900

    M0: DUT wrapper, verisim build flow, RTL-only RVProcTest harness

---
+++ dut.h
@@ -0,0 +1,31 @@
+#ifndef DUT_H
+#define DUT_H
+
+#include "RVProc.h"
+#include "io/RVProc_io.h"
+#include "io/ExtMem.h"
+#include "verisim.h"
+
+#ifdef VERISIM_TRACE
+#include <verilated_fst_c.h>
+#endif
+
+struct DUT {
+	VDUT *vdut;
+#ifdef VERISIM_TRACE
+	VerilatedFstC *tfp;
+	vluint64_t trace_tick;
+#endif
+
+	DUT();
+	~DUT();
+
+	void init(RV_AType pc, RV_UType sp, RV_UType dtb);
+	bool step(MEMCTLPin *mpin);
+	void sync(CoreState &cpu);
+
+	template <int N> void write(AXI4L::CH *ch);
+	template <int N> void read(AXI4L::CH *ch);
+};
+
+#endif // DUT_H
